3e823e7ff2414c4217f1338ed14ef86ab57bda4c
Chris Trevino <darthtrevino@gmail.com>
2018-05-31 10:29:06 -0700

    Rename example folders

--- packages/core/src/__tests__/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DERPDERP } from '../index'
-
-describe('The Derpderp core', () => {
-	it('has a value', () => {
-		expect(DERPDERP).toEqual(1)
-	})
-})
--- packages/core/src/index.ts
+++
@@ -1,1 +0,0 @@
-export const DERPDERP = 1
