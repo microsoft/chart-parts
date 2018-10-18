5acb349c3db97233f3079fe4fc46fbd225cf6066
Chris Trevino <darthtrevino@gmail.com>
2018-10-18 09:24:15 -0700

    feat: add population pyramid example to mobile

--- packages/examples/mobile/src/App.js
+++ packages/examples/mobile/src/App.js
@@ -5,13 +5,14 @@
 import * as React from 'react'
 import Chart from './BarChart'
 import Charts from './Chart'
+import PopulationPyramid from './PopulationPyramid'
 import { StyleSheet, View } from 'react-native'
 
 console.log('Render App', Chart, Charts)
 
 const App = () => (
 	<View style={styles.container}>
-		<Charts />
+		<PopulationPyramid />
 	</View>
 )
 
