f8d46583c678279a367c176ae94219ff78a9f471
Chris Trevino <darthtrevino@gmail.com>
2019-08-19 21:52:39 -0700

    build: build script update

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-npx tsc
+npx tsc -b
 npx babel --config-file=../../../babel.config.js lib --ignore="src/**/__tests__/**" --out-dir dist/esm &
 npx babel --config-file=../../../babel.config.cjs.js lib --ignore="src/**/__tests__/**" --out-dir dist/cjs &
 wait
