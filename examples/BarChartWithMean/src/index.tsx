66511eda43d538da0b24835ac9b0928003bd4c1c
Chris Trevino <darthtrevino@gmail.com>
2019-09-17 15:59:19 -0700

    Updates

--- examples/BarChartWithMean/src/index.tsx
+++ examples/BarChartWithMean/src/index.tsx
@@ -1,5 +1,5 @@
 
-	import React from 'react'
+	import React, { memo } from 'react'
 	import ReactDOM from 'react-dom'
 	import Example from './example'
 	import { ChartingProvider } from '@chart-parts/react'
--- styles-44ad2c58352c08283f8e.js
+++ styles-44ad2c58352c08283f8e.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[9],{445:function(n,w,o){}}]);
-//# sourceMappingURL=styles-789ce3bbde0b8de1e649.js.map
+//# sourceMappingURL=styles-44ad2c58352c08283f8e.js.map
