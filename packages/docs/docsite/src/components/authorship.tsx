877c41fe7d8ebd0a966359a9d8a48f8735346be5
Chris Trevino <darthtrevino@gmail.com>
2019-09-18 13:22:49 -0700

    docs: use github, twitter icons in the authorship component

--- packages/docs/docsite/src/components/authorship.tsx
+++ packages/docs/docsite/src/components/authorship.tsx
@@ -2,7 +2,10 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import React, { memo } from 'react'
+import React, { memo, useMemo } from 'react'
+import { GoMarkGithub as GitHubIcon } from 'react-icons/go'
+import { FaTwitter as TwitterIcon } from 'react-icons/fa'
+import styled from 'styled-components'
 
 export interface AuthorshipProps {
 	name: string
@@ -11,18 +14,41 @@ export interface AuthorshipProps {
 }
 export const Authorship: React.FC<AuthorshipProps> = memo(
 	({ name, twitter, github }) => {
+		const twitterUrl = useMemo(
+			() => twitter && `https://twitter.com/${twitter}`,
+			[twitter]
+		)
+		const githubUrl = useMemo(() => github && `https://github.com/${github}`, [
+			github,
+		])
 		return (
-			<sub>
-				by: {name}&nbsp;
+			<span>
+				<By>by </By>
+				<Name>{name}</Name>&nbsp;
 				{twitter ? (
 					<>
-						<a href={`https://twitter.com/${twitter}`}>Twitter</a>
+						<a href={twitterUrl}>
+							<TwitterIcon size={12} />
+						</a>
 						&nbsp;
 					</>
 				) : null}
-				{github ? <a href={`https://github.com/${github}`}>GitHub</a> : null}
-			</sub>
+				{github ? (
+					<a href={githubUrl}>
+						<GitHubIcon size={12} />
+					</a>
+				) : null}
+			</span>
 		)
 	}
 )
 Authorship.displayName = 'Authorship'
+
+const By = styled.span`
+	font-size: 10px;
+	font-weight: 100;
+`
+const Name = styled.span`
+	font-size: 12px;
+	font-weight: 300;
+`
