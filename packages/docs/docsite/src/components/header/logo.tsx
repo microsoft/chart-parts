df84414a89ad3a193f2962cc34d5cc8b0e33d509
Amber Hoak <amhoak@microsoft.com>
2019-11-07 15:27:30 -0800

    fix: add headers to added files

--- packages/docs/docsite/src/components/header/logo.tsx
+++ packages/docs/docsite/src/components/header/logo.tsx
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+
 import React from 'react'
 interface Logo {
 	height: number
