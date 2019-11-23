2a35f226a6ee94370f8c3e8f73784eb0aeba9afe
darthtrevino <darthtrevino@users.noreply.github.com>
2019-11-23 03:02:43 +0000

    Automated deployment: Sat Nov 23 03:02:43 UTC 2019 89ebc1a5a2dfaeb5944aa07e6ab81e4429a8f906

--- apidocs/renderers/react-svg/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"128":"Class","2048":"Method"},"rows":[{"id":0,"kind":128,"name":"Renderer","url":"classes/renderer.html","classes":"tsd-kind-class"},{"id":1,"kind":2048,"name":"render","url":"classes/renderer.html#render","classes":"tsd-kind-method tsd-parent-kind-class","parent":"Renderer"}]};
--- examples/LineChart/src/example.ts
+++
@@ -1,5 +0,0 @@
-/*!
- * Copyright (c) Microsoft. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project.
- */
-export { LineChart as default } from './LineChart'
