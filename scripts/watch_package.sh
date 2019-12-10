d6769dd3248b0c269b68e6fd32a94f3781abeeb2
Chris Trevino <darthtrevino@gmail.com>
2019-12-10 11:33:01 -0800

    build(scripts): update bash scripts to emit errors

--- scripts/watch_package.sh
+++ scripts/watch_package.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 tsc -w --preserveWatchOutput&
 npx babel --config-file=../../../babel.config.cjs.js lib --out-dir dist/cjs -w --verbose&
 npx babel --config-file=../../../babel.config.js lib --out-dir dist/esm -w --verbose
