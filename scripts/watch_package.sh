dd4b60775ecfbe78b5de3bd61479d596c113c9af
Chris Trevino <chtrevin@microsoft.com>
2020-06-05 11:16:51 -0700

    build: remove conf covered by build scripts

--- scripts/watch_package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-tsc -w --preserveWatchOutput&
-npx babel --config-file=../../../babel.config.cjs.js lib --out-dir dist/cjs -w --verbose&
-npx babel --config-file=../../../babel.config.js lib --out-dir dist/esm -w --verbose
