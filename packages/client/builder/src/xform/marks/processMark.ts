f0467cadd2b5eee79becd0a07c26906986503713
Chris Trevino <chtrevin@microsoft.com>
2020-06-05 19:15:33 -0700

    build: update tsconfigs

--- packages/client/builder/src/xform/marks/processMark.ts
+++ packages/client/builder/src/xform/marks/processMark.ts
@@ -129,7 +129,11 @@ function createFacetedData(
 	facetSourceTable: any[],
 	markSourceTable: any[],
 ): FacetedData {
+	if (!facet.groupBy) {
+		throw new Error('facet.groupBy must be defined')
+	}
 	const getKey = keyGetter(facet.groupBy)
+
 	const facetMap = new Map<string, DataFacet>()
 	const markMap = new Map<string, any>()
 	const facets: DataFacet[] = []
