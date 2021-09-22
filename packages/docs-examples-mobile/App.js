eb0b639bb55b18a5a5adf598b5d9b076e3e70362
Chris Trevino <chtrevin@microsoft.com>
2021-09-22 11:48:07 -0700

    fix: mobile tests

--- packages/docs-examples-mobile/App.js
+++ packages/docs-examples-mobile/App.js
@@ -3,7 +3,7 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 
-import {Fragment} from 'react';
+import React from 'react';
 import {
 	SafeAreaView,
 	StyleSheet,
@@ -23,7 +23,7 @@ import {
 
 const App = () => {
 	return (
-		<Fragment>
+		<>
 			<StatusBar barStyle="dark-content" />
 			<SafeAreaView>
 				<ScrollView
@@ -66,7 +66,7 @@ const App = () => {
 					</View>
 				</ScrollView>
 			</SafeAreaView>
-		</Fragment>
+		</>
 	);
 };
 
